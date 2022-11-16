836facc23228163185c81664aebe958d9723d1f6
William Kay Kephart <wllmkphrt@gmail.com>
2022-11-16 12:42:27 -0500

    commit tree 0402858d9f0c8ee82b64b5cc888cd3dcb159a43c

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.getElementById('main');
+main.remove();
+const newHeader = document.createElement('H1');
+newHeader.id = "victory";
+newHeader.textContent = "William Kephart is the champion"
+document.head.append(newHeader);
